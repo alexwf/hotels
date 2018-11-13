e713db32146396476270cf45ffbf1bc187b3e3f4
Alex Friedrichsen <alexw.f@hotmail.com>
2018-11-12 23:34:03 -0200

    Melhorias área pública e administração
    Atualização dos componentes de acordo com a nova versão do bootstrap.
    Correções de layout.
    Implementação dos detalhes na área pública.

--- SisEventos/Controllers/EventosController.cs
+++ SisEventos/Controllers/EventosController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SisEventos.Models;
 
 namespace SisEventos.Controllers
@@ -21,6 +22,22 @@ namespace SisEventos.Controllers
         {
             List<Evento> eventos = db.Eventos.ToList();
             return View(eventos);
+        }
+
+        [HttpGet]
+        public IActionResult Detail(long id)
+        {
+            Evento evento = this.db.Eventos
+                                  .Include(m => m.Curso)
+                                  .Where(x => x.Id == id)
+                                  .FirstOrDefault();
+
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            return View(evento);
         }
     }
 }
