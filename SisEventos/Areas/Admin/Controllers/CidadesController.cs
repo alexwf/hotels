b650658a79f6987a49248338d5d0f3beb32f44fc
Alex Friedrichsen <alexw.f@hotmail.com>
2018-11-18 10:01:26 -0200

    Cidade

--- SisEventos/Areas/Admin/Controllers/CidadesController.cs
+++ SisEventos/Areas/Admin/Controllers/CidadesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SisEventos.Models;
+using SisEventos.ViewModels;
 
 namespace SisEventos.Areas.Admin.Controllers
 {
@@ -20,21 +21,42 @@ namespace SisEventos.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-            Cidade cidade = new Cidade();
-            return View(cidade);
+            CidadeVM vm = new CidadeVM();
+
+            return View(vm);
         }
 
         [HttpPost]
-        public IActionResult Create(Cidade cidade)
+        public IActionResult Create(CidadeVM vm)
         {
             if (ModelState.IsValid)
             {
-                db.Cidades.Add(cidade);
-                db.SaveChanges();
+                Cidade cidade = new Cidade();
+
+                cidade.Nome = vm.Nome;
+                this.db.Cidades.Add(cidade);
+                this.db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(cidade);
+            return View(vm);
         }
-    }
+
+        [HttpGet]
+        public IActionResult Edit(long Id)
+        {
+            Cidade cidade = this.db.Cidades
+                                    .Where(x => x.Id == Id)
+                                    .FirstOrDefault();
+
+
+            CidadeVM vm = new CidadeVM();
+            //vm.Nome = Cidade.Nome;
+        }
+
+        [HttpPost]
+        public IActionResult Edit(long Id, CidadeVM vm)
+        {
+
+        }
 }
